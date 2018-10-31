85431c66a1e84268886ca7ea18692610478e339b
Diego Forero <gollum23@gmail.com>
2018-10-30 20:16:12 -0500

    :wrench: transactions models

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 from explorer.models import TimeStampModel
@@ -48,6 +49,58 @@ class Transaction(TimeStampModel):
         on_delete=models.CASCADE,
         null=True
     )
+    block_number = models.IntegerField(
+        null=True
+    )
+    cumulative_gas_used = models.DecimalField(
+        null=True
+    )
+    created_contract_address_hash = models.ForeignKey(
+        'address.Address',
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='transaction_contract_address'
+    )
+    error = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True
+    )
+    from_address_hash = models.ForeignKey(
+        'address.Address',
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='transaction_from_address'
+    )
+    gas = models.DecimalField()
+    gas_price = models.DecimalField()
+    gas_used = models.DecimalField(
+        null=True
+    )
+    hash = models.CharField(
+        max_length=255
+    )
+    index = models.IntegerField(
+        null=True
+    )
+    input = models.TextField()
+    internal_transactions_indexed_at = models.BigIntegerField(
+        null=True
+    )
+    nonce = models.IntegerField()
+    r = models.DecimalField()
+    s = models.DecimalField()
+    status = models.IntegerField(
+        null=True
+    )
+    to_address_hash = models.ForeignKey(
+        'address.Address',
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='transaction_to_address'
+    )
+    v = models.IntegerField()
+    value = models.DecimalField()
 
 
 class Fork(TimeStampModel):
@@ -56,18 +109,27 @@ class Fork(TimeStampModel):
     `hash` with a non-consensus uncle Block instead of the consensus block
     linked in the Transaction block_hash
     --Fields--
-    * hash -> hash of contents of this transaction | String
-    * index -> Index of this transaction in `uncle` Block | Int
-    * transaction -> Data shared between all forks and the consensus
+    * hash ->  Data shared between all forks and the consensus
     transaction | Foreign Key
-    * uncle_has -> uncle Block in which this transaction was mined/validated
+    * index -> Index of this transaction in `uncle` Block | Int
+    * uncle_hash -> uncle Block in which this transaction was mined/validated
     """
+
+    hash = models.ForeignKey(
+        'transactions.Transaction',
+        on_delete=models.CASCADE
+    )
+    index = models.IntegerField,
+    uncle_hash = models.ForeignKey(
+        'blocks.Block',
+        on_delete=models.CASCADE
+    )
 
 
 class InternalTransaction(TimeStampModel):
     """
     * call_type -> Type of call, None when type is not `call` | String
-    * created_contract_code -> The code of the contract that was created when
+    --* created_contract_code -> The code of the contract that was created when
     `call_type` is created | Text
     * created_contract_address_hash -> Address Foreign Key when Address is of
     a contract | Foreign Key
@@ -90,3 +152,50 @@ class InternalTransaction(TimeStampModel):
     | String
     * value -> value transferred from `from_address` to `to_address` | Decimal
     """
+
+    call_type = models.CharField(
+        max_length=255,
+        null=True
+    )
+    created_contract_address_hash = models.ForeignKey(
+        'address.Address',
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='internal_transaction_contract_address'
+    )
+    error = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True
+    )
+    from_address_hash = models.ForeignKey(
+        'address.Address',
+        on_delete=models.CASCADE,
+        related_name='internal_transaction_from_address'
+    )
+    gas = models.DecimalField(
+        null=True
+    )
+    gas_used = models.DecimalField(
+        null=True
+    )
+    index = models.IntegerField()
+    init = models.TextField()
+    input = models.TextField()
+    output = models.TextField()
+    to_address_hash = models.ForeignKey(
+        'address.Address',
+        on_delete=models.CASCADE,
+        related_name='internal_transaction_to_address'
+    )
+    trace_address = ArrayField(
+        models.IntegerField()
+    )
+    transaction_hash = models.ForeignKey(
+        'transactions.Transaction',
+        on_delete=models.CASCADE
+    )
+    type = models.CharField(
+        max_length=255
+    )
+    value = models.DecimalField()
